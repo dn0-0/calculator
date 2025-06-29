1fc692f268ed255f910e8dd7fa62dcf986168580
dnl <z25703853@gmail.com>
2025-06-29 21:58:29 +0330

    first commit

---
+++ app.py
@@ -0,0 +1,20 @@
+num1 = int(input('first number'))
+operator = int(input('+,-,/,*'))
+num2 = int(input('second number'))
+
+result = 0
+
+try:
+    match operator:
+        case '+':
+            result = num1 + num2
+        case '-':
+            result = num1 + num2
+        case '/':
+            result = num1 + num2
+        case '*':
+            result = num1 + num2
+        case _:
+            raise Exception
+except:
+    print('something is wrong')
